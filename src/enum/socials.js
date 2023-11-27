e5cefc6999d735e3a077185f3dbfcb13c3330e1a
annamarieee <115216892+annamarieee@users.noreply.github.com>
2023-11-27 16:55:03 +0800

    updated social_links

--- src/enum/socials.js
+++ src/enum/socials.js
@@ -1,7 +1,7 @@
 const SOCIAL_LINKS = {
   Gmail: "https://bit.ly/3DRvthi",
   Facebook: "https://www.facebook.com/anna.marasigan17/",
-  LinkedIn: "https://www.linkedin.com/in/anna-marie-marasigan-a25b9620b/",
+  LinkedIn: "https://www.linkedin.com/in/anna-marie-marasigan17/",
 };
 
 export default SOCIAL_LINKS;
